409d1f9943eee86380d865734018a11e8a0610b8
Dark-super-me <87744886+Dark-super-me@users.noreply.github.com>
2021-09-18 16:26:15 +0530

    Create Hiboss.py

---
+++ Hiboss.py
@@ -0,0 +1,11 @@
+import asyncio
+import os
+import re
+import time
+from datetime import datetime as dt
+# Rate this at @Animes_Encoded 
+
+@ultroid_cmd(pattern="hiboss")
+async def hiboss(ult):
+
+ 
