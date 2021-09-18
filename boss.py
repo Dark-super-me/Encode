47d679f30f250222a9ea42b0ad2d8b7ddbaa7273
Dark-super-me <87744886+Dark-super-me@users.noreply.github.com>
2021-09-18 16:46:21 +0530

    Update boss.py

--- boss.py
+++ boss.py
@@ -1,3 +1,9 @@
+import time
+import os 
+import asyncio 
+import re
+
+
 @ultroid_cmd(pattern="hiboss ?(.*)")
 async def hi(event):
     giveVar = event.text
@@ -8,3 +14,7 @@ async def hi(event):
         event,
         f"{ult}✨✨{ult}✨{ult}{ult}{ult}\n{ult}✨✨{ult}✨✨{ult}✨\n{ult}{ult}{ult}{ult}✨✨{ult}✨\n{ult}✨✨{ult}✨✨{ult}✨\n{ult}✨✨{ult}✨{ult}{ult}{ult}\n☁☁☁☁☁☁☁☁",
     )
+    await asyncio.sleep(1)
+    await edit_or_reply("Boss")
+
+    
